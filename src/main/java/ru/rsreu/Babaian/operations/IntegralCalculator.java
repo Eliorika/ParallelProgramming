16e6b91c27710a8bbfb10c78f3552067c1226adb
Eliorika <arina.pavlovna.20@gmail.com>
2023-10-08 16:34:48 +0300

    lab5

--- src/main/java/ru/rsreu/Babaian/operations/IntegralCalculator.java
+++ src/main/java/ru/rsreu/Babaian/operations/IntegralCalculator.java
@@ -3,6 +3,11 @@ package ru.rsreu.Babaian.operations;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 @RequiredArgsConstructor
 public class IntegralCalculator implements Runnable{
     private final double tolerance;
@@ -10,19 +15,25 @@ public class IntegralCalculator implements Runnable{
 
     @Getter
     private double elapsedTimeSeconds;
+    private ExecutorService executorService = Executors.newCachedThreadPool();
 
-    public double calculateIntegralFrom0To1() throws InterruptedException {
-        return calculateIntegral(0,1);
+    public double calculateIntegralFrom0To1() throws InterruptedException, ExecutionException {
+        return calculateIntegralWithTollerance(0,1);
     }
 
     public void writeMessage(int percent){
         System.out.println("Progress: " + percent + "%");
     }
 
-    private double calculateIntegral(double a, double b) throws InterruptedException {
+    private double calculateIntegralWithTollerance(double a, double b) throws InterruptedException, ExecutionException {
         double integral = 0.0;
         double previous;
         int n = 1;
+        Future<Double> integral1 = calculateIntegralFuture(a, b, n);
+        n*=2;
+        Future<Double> integral2 = calculateIntegralFuture(a, b, n);
+
+
         double progressStep = Math.pow(this.tolerance, 1.0/this.messages);
         double progressBorder = progressStep;
         int percent = 0;
@@ -35,18 +46,14 @@ public class IntegralCalculator implements Runnable{
             if (Thread.currentThread().isInterrupted()) {
                 throw new InterruptedException();
             }
-            double h = (b - a) / n;
-            double sum = 0.0;
-
-            for (int i = 1; i < n; i++) {
-                double x = a + i * h;
-                sum += Math.sin(x) * x;
-            }
 
             previous = integral;
-            integral = (h / 2.0) * (Math.sin(a) * a + Math.sin(b) * b + 2.0 * sum);
 
+            integral = integral1.get();
+            integral1 = integral2;
             n *= 2;
+            integral2 = calculateIntegralFuture(a, b, n);
+
 
             if (progressBorder > Math.abs(integral - previous)) {
                 percent+=percentStep;
@@ -64,13 +71,29 @@ public class IntegralCalculator implements Runnable{
         return integral;
     }
 
+    private Future<Double> calculateIntegralFuture(double a, double b, int n){
+        return executorService.submit(() -> calculateIntegral(a, b, n));
+    }
+
+    private double calculateIntegral(double a, double b, int n){
+        double h = (b - a) / n;
+        double sum = 0.0;
+        for (int i = 1; i < n; i++) {
+            double x = a + i * h;
+            sum += Math.sin(x) * x;
+        }
+        return (h / 2.0) * (Math.sin(a) * a + Math.sin(b) * b + 2.0 * sum);
+    }
+
     @Override
     public void run() {
         try {
             double answer = calculateIntegralFrom0To1();
             System.out.println("Answer: " + answer);
         } catch (InterruptedException e) {
-            System.out.println("Thread is interrupted");
+            System.out.println("Thread is interrupted manually");
+        } catch (Exception e){
+            System.out.println("Thread id interrupted");
         }
     }
 }
